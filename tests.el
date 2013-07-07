3b8be572a3e6c16e82fcbe0537b9ba452c289009
Mika Vilpas <mika.vilpas@gmail.com>
2013-07-08 00:08:59 +0300

    Comment out useless tests.

--- tests.el
+++ tests.el
@@ -1,14 +1,14 @@
 
 
-(omnisharp--display-autocomplete-suggestions
- (list (json-read-from-string
- "{\"CompletionText\":\"ToString()\",\"Description\":\"string
- ToString();\\nMethodSystem.String\\nReturns a System.String
- representation of the value of the current instance. \\nReturns: \\nA
- System.String representation of the current
- instance.\\nRemarks:\\n\\n This method overrides
- System.Object.ToString.01.0.5000.02.0.0.04.0.0.0\",\"DisplayText\":\"string
- ToString()\"}")))
+;; (omnisharp--display-autocomplete-suggestions
+;;  (list (json-read-from-string
+;;  "{\"CompletionText\":\"ToString()\",\"Description\":\"string
+;;  ToString();\\nMethodSystem.String\\nReturns a System.String
+;;  representation of the value of the current instance. \\nReturns: \\nA
+;;  System.String representation of the current
+;;  instance.\\nRemarks:\\n\\n This method overrides
+;;  System.Object.ToString.01.0.5000.02.0.0.04.0.0.0\",\"DisplayText\":\"string
+;;  ToString()\"}")))
 
 ;; For quick testing
 (define-key evil-insert-state-map
@@ -32,6 +32,6 @@
     (omnisharp-go-to-definition)))
 
 ;; Should be 19
-(omnisharp--get-max-item-length '("lontlnitaa" "llinatilnailantonta" "lonta" "lol"))
+;; (omnisharp--get-max-item-length '("lontlnitaa" "llinatilnailantonta" "lonta" "lol"))
 
-(omnisharp-reload-solution)
+;; (omnisharp-reload-solution)
