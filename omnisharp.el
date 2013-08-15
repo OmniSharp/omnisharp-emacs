1546ad03e4d0578fe54cee03f949f806dde4816e
Mika Vilpas <mika.vilpas@gmail.com>
2013-08-15 21:43:54 +0300

    Fix Package-Requires versions to official versions
    Used to be snapshot versions

--- omnisharp.el
+++ omnisharp.el
@@ -3,7 +3,7 @@
 ;; Author: Mika Vilpas
 ;; Version: 0.1
 ;; Url: https://github.com/sp3ctum/omnisharp-emacs
-;; Package-Requires: ((json "1.2") (dash "20130712.2307") (popup "20130117.1954") (auto-complete "20130122.1551") (flycheck "20130707.1121"))
+;; Package-Requires: ((json "1.2") (dash "1.8.0") (popup "0.5") (auto-complete "1.4") (flycheck "0.14-cvs"))
 ;; Keywords: csharp c# IDE auto-complete intellisense
 
 ;;; Commentary:
