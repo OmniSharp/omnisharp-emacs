c4381c52dbea8c41d446aa1231f9e018b8a61c39
Mika Vilpas <mika.vilpas@gmail.com>
2013-08-18 10:49:10 +0300

    Make example-config-for-evil-mode.el use omnisharp-mode-map

--- example-config-for-evil-mode.el
+++ example-config-for-evil-mode.el
@@ -1,37 +1,37 @@
 ;; Example evil-mode config
 
-(define-key evil-insert-state-map
+(evil-define-key 'insert omnisharp-mode-map
   (kbd "M-.")
   (lambda () (interactive)
     (omnisharp-auto-complete)))
 
-(define-key evil-normal-state-map
+(evil-define-key 'normal omnisharp-mode-map
   (kbd "<f12>")
   (lambda () (interactive)
     (omnisharp-go-to-definition)))
 
-(define-key evil-normal-state-map
+(evil-define-key 'normal omnisharp-mode-map
   (kbd "g u")
   (lambda () (interactive)
     (omnisharp-find-usages)))
 
-(define-key evil-normal-state-map
+(evil-define-key 'normal omnisharp-mode-map
   (kbd "g o")
   (lambda () (interactive)
     (omnisharp-go-to-definition)))
 
-(define-key evil-normal-state-map
+(evil-define-key 'normal omnisharp-mode-map
   (kbd "g r")
   (lambda () (interactive)
     (omnisharp-run-code-action-refactoring)))
 
-(define-key evil-normal-state-map
+(evil-define-key 'normal omnisharp-mode-map
   (kbd ", i") 'omnisharp-current-type-information)
 
-(define-key evil-normal-state-map
+(evil-define-key 'normal omnisharp-mode-map
   (kbd ", b") 'omnisharp-browse-type)
 
-(evil-define-key 'insert csharp-mode-map
+(evil-define-key 'insert omnisharp-mode-map
   (kbd ".") 'omnisharp-add-dot-and-auto-complete)
 
 ;; Speed up auto-complete on mono drastically. This comes with the
