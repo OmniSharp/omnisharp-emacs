18f70e0483ddadd215f7266d155ce08bc78c176f
Mika Vilpas <mika.vilpas@gmail.com>
2013-08-18 10:50:21 +0300

    Simplify example-config-for-evil-mode.el mappings
    Removed redundant lambdas

--- example-config-for-evil-mode.el
+++ example-config-for-evil-mode.el
@@ -1,29 +1,19 @@
 ;; Example evil-mode config
 
 (evil-define-key 'insert omnisharp-mode-map
-  (kbd "M-.")
-  (lambda () (interactive)
-    (omnisharp-auto-complete)))
+  (kbd "M-.") (omnisharp-auto-complete))
 
 (evil-define-key 'normal omnisharp-mode-map
-  (kbd "<f12>")
-  (lambda () (interactive)
-    (omnisharp-go-to-definition)))
+  (kbd "<f12>") (omnisharp-go-to-definition))
 
 (evil-define-key 'normal omnisharp-mode-map
-  (kbd "g u")
-  (lambda () (interactive)
-    (omnisharp-find-usages)))
+  (kbd "g u") (omnisharp-find-usages))
 
 (evil-define-key 'normal omnisharp-mode-map
-  (kbd "g o")
-  (lambda () (interactive)
-    (omnisharp-go-to-definition)))
+  (kbd "g o") (omnisharp-go-to-definition))
 
 (evil-define-key 'normal omnisharp-mode-map
-  (kbd "g r")
-  (lambda () (interactive)
-    (omnisharp-run-code-action-refactoring)))
+  (kbd "g r") (omnisharp-run-code-action-refactoring))
 
 (evil-define-key 'normal omnisharp-mode-map
   (kbd ", i") 'omnisharp-current-type-information)
