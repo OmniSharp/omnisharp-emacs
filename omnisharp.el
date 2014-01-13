423b746bf7d6d466482e3829ae99f2d1b7efef7e
Mika Vilpas <mika.vilpas@gmail.com>
2014-01-13 09:27:12 +0200

    Bump version 1.5 -> 1.6

--- omnisharp.el
+++ omnisharp.el
@@ -1,7 +1,7 @@
 ;;; omnisharp.el --- Omnicompletion (intellisense) and more for C#
 ;; Copyright (C) 2013 Mika Vilpas (GPLv3)
 ;; Author: Mika Vilpas
-;; Version: 1.5
+;; Version: 1.6
 ;; Url: https://github.com/sp3ctum/omnisharp-emacs
 ;; Package-Requires: ((json "1.2") (dash "1.8.0") (popup "0.5") (auto-complete "1.4") (flycheck "0.13"))
 ;; Keywords: csharp c# IDE auto-complete intellisense
