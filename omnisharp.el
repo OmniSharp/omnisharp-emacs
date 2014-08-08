73647de330b5dc1b341b20f4aab7808728c1495a
Mika Vilpas <mika.vilpas@gmail.com>
2014-08-08 11:07:50 +0300

    Make windows curl tmp file default to a writable location
    It was originally set to C:/foo.cs which isn't writable in some
    configurations. The user profile directory should be writable for all
    users. I hope.

--- omnisharp.el
+++ omnisharp.el
@@ -164,7 +164,9 @@ information.")
 ;; Note that emacs seems to internally expect windows paths to have
 ;; forward slashes.
 (defcustom omnisharp--windows-curl-tmp-file-path
-  "C:/omnisharp-tmp-file.cs"
+  (omnisharp--convert-backslashes-to-forward-slashes
+   (concat (getenv "USERPROFILE")
+           "/omnisharp-tmp-file.cs"))
   "The full file path where to save temporary stuff that gets sent to
 the OmniSharp API. Only used on Windows.
 Must be writable by the current user."
