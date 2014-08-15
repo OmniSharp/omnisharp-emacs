f650e6bf353ab5d9affc946d6e00059e6bf13a19
Mika Vilpas <mika.vilpas@gmail.com>
2014-08-15 21:38:28 +0300

    Add deault binding for "find implementations"
    I'm not sure why this was missing. The "find implementations"
    functionality has been around for ages.

--- example-config-for-evil-mode.el
+++ example-config-for-evil-mode.el
@@ -3,6 +3,7 @@
 (evil-define-key 'insert omnisharp-mode-map (kbd "M-.") 'omnisharp-auto-complete)
 (evil-define-key 'normal omnisharp-mode-map (kbd "<f12>") 'omnisharp-go-to-definition)
 (evil-define-key 'normal omnisharp-mode-map (kbd "g u") 'omnisharp-find-usages)
+(evil-define-key 'normal omnisharp-mode-map (kbd "g I") 'omnisharp-find-implementations) ; g i is taken
 (evil-define-key 'normal omnisharp-mode-map (kbd "g o") 'omnisharp-go-to-definition)
 (evil-define-key 'normal omnisharp-mode-map (kbd "g r") 'omnisharp-run-code-action-refactoring)
 (evil-define-key 'normal omnisharp-mode-map (kbd "g f") 'omnisharp-fix-code-issue-at-point)
