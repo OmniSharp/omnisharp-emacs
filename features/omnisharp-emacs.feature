1cabc75736ed4f1c587b82ef91c16e2269adda15
Mika Vilpas <mika.vilpas@gmail.com>
2014-10-16 23:40:24 +0300

    Add integration test for omnisharp-fix-usings
    It currently requires the omnisharp server to be running, but it works!

--- features/omnisharp-emacs.feature
+++ features/omnisharp-emacs.feature
@@ -1,11 +1,26 @@
-Feature: Do Some things
-  In order to do something
+Feature: Fix usings
+  In order to code without having to worry about using statements
   As a user
-  I want to do something
+  I want to automatically insert missing using statements based on the symbols in the current file
 
-  Scenario: Do Something
-    Given I have "something"
-    When I have "something"
-    Then I should have "something"
-    And I should have "something"
-    But I should not have "something"
+  Background:
+    # TODO start omnisharp server somehow
+    Given I open temp file "some-file.cs"
+    And I bind key "C-c i" to "omnisharp-fix-usings"
+
+  Scenario: Insert missing namespace import
+    When I insert:
+      """
+      public class Awesome {
+          StringWriter writer;
+      }
+      """
+    And I press "C-c i"
+    Then I should see, ignoring line endings:
+      """
+      using System.IO;
+
+      public class Awesome {
+          StringWriter writer;
+      }
+      """
--- features/step-definitions/omnisharp-emacs-steps.el
+++ features/step-definitions/omnisharp-emacs-steps.el
@@ -5,27 +5,14 @@
 ;; Find some included steps at
 ;; https://github.com/ecukes/espuds/blob/master/espuds.el#L130
 
-(Given "^I have \"\\(.+\\)\"$"
-       (lambda (something)
-         ;; ...
-         ))
+(Given "^I bind key \"\\([^\"]+\\)\" to \"\\([^\"]+\\)\"$"
+       (lambda (key fn-name)
+         (global-set-key (kbd key) (intern fn-name))))
 
-(When "^I have \"\\(.+\\)\"$"
-  (lambda (something)
-    ;; ...
-    ))
-
-(Then "^I should have \"\\(.+\\)\"$"
-  (lambda (something)
-    ;; ...
-    ))
-
-(And "^I have \"\\(.+\\)\"$"
-  (lambda (something)
-    ;; ...
-    ))
-
-(But "^I should not have \"\\(.+\\)\"$"
-  (lambda (something)
-    ;; ...
-    ))
+(Then "^I should see, ignoring line endings\\(?: \"\\(.+\\)\"\\|:\\)$"
+      "Asserts that the current buffer includes some text. Ignores
+line endings, so windows CRLF is considered the same as Unix LF."
+      (lambda (expected)
+        (let ((actual (s-replace (string ?\C-m) (string ?\C-j) (buffer-string)))
+              (message "Expected '%s' to be part of '%s', but was not."))
+          (cl-assert (s-contains? expected actual) nil message expected actual))))
