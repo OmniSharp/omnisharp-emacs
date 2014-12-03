38bfba48f94541e4881d162e9e7f00402c3e98c8
Mika Vilpas <mika.vilpas@gmail.com>
2014-12-03 18:15:46 +0200

    Attempt to run unit tests on travis too

--- run-entire-travis-build.sh
+++ run-entire-travis-build.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
-echo "./run-integration-tests.sh"
-./run-integration-tests.sh
-
 echo "./run-tests.sh"
 ./run-tests.sh
+
+echo "./run-integration-tests.sh"
+./run-integration-tests.sh
 
 echo "./run-melpa-build-test.sh"
 ./run-melpa-build-test.sh
