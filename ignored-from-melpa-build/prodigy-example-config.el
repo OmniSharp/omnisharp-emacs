395809ccfc02b346d4c13f8181f2429937a6ef12
Mika Vilpas <mika.vilpas@gmail.com>
2015-04-30 21:30:29 +0300

    Add example config for prodigy
    I'll lose it if I don't save it now, and it could be useful for others
    by an odd chance.

---
+++ ignored-from-melpa-build/prodigy-example-config.el
@@ -0,0 +1,65 @@
+;; If you use the prodigy plugin for emacs, you can quickly adapt this
+;; file to set up emacs as an interface to start background processes
+;; and run test for this project.
+;;
+;; To use this file as such do the following:
+;; - Configure the file and set the paths right
+;; - Load this file's code into emacs somehow, e.g. from your init file do
+;;     (load-file "/home/mika/git/omnisharp-emacs/ignored-from-melpa-build/prodigy-example-config.el")
+;; - Then run M-x prodigy. You will be shown the services defined in
+;;   this file. Start a service with "s" and view its output with "$"
+;;   (once it starts generating output). For more info, see the
+;;   prodigy site below
+;;
+;; For prodigy:
+;; https://github.com/rejeep/prodigy.el
+(require 'prodigy)
+
+;; omnisharp-emacs development hacks
+(setenv "PATH" (concat "/opt/monodevelop/bin/:"
+                       (getenv "PATH")
+                       ":/home/mika/.cask/bin"
+                       "/home/mika/bin/"))
+
+(let ((omni-dir "/home/mika/git/omnisharp-emacs/")
+      (todomvc-lol-dir "/home/mika/git/todomvc-lol/")
+      (lein-dir "/home/mika/bin/")
+      (chat-dir "/home/mika/git/clojure-practice/chat-lol/"))
+  (prodigy-define-service
+    :name "OmniSharpServer for integration tests"
+    :command (concat omni-dir "start-omnisharp-server-for-integration-tests.sh")
+    :cwd omni-dir
+    :stop-signal 'kill
+    :kill-process-buffer-on-stop t
+    :truncate-output 200
+    :tags '(omnisharp))
+
+  (prodigy-define-service
+    :name "omnisharp-emacs integration tests"
+    :command (concat omni-dir "run-integration-tests.sh")
+    :cwd omni-dir
+    :stop-signal 'kill
+    :kill-process-buffer-on-stop t
+    :truncate-output 200
+    :tags '(omnisharp))
+
+  (prodigy-define-service
+    :name "omnisharp-emacs unit tests"
+    :command (concat omni-dir "run-tests.sh")
+    :cwd omni-dir
+    :stop-signal 'kill
+    :kill-process-buffer-on-stop t
+    :truncate-output 200
+    :tags '(omnisharp))
+
+  (prodigy-define-service
+    :name "omnisharp-emacs installation test"
+    :command (concat omni-dir "run-melpa-build-test.sh")
+    :cwd omni-dir
+    :stop-signal 'kill
+    :kill-process-buffer-on-stop t
+    :truncate-output 200
+    :tags '(omnisharp)))
+
+(add-to-list 'evil-emacs-state-modes 'prodigy-mode)
+(provide 'prodigy-example-config)
