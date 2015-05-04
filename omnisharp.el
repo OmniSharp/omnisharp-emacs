d9812f743856a86c238840b895c560ab4cec0ef2
Juergen Hoetzel <juergen@archlinux.org>
2015-05-04 22:12:08 +0200

    Add flycheck checkers in correct order
    csharp-omnisharp-curl should be the first checker, because it's the
    checker which has :next-checkers defined.

--- omnisharp.el
+++ omnisharp.el
@@ -127,11 +127,9 @@ server backend."
   (omnisharp--start-omnisharp-server-for-solution-in-parent-directory)
 
   ;; These are selected automatically when flycheck is enabled
-  (--each '(csharp-omnisharp-curl
-            csharp-omnisharp-curl-code-issues
-            csharp-omnisharp-curl-semantic-errors)
-
-    (add-to-list 'flycheck-checkers it)))
+  (nconc flycheck-checkers '(csharp-omnisharp-curl
+			     csharp-omnisharp-curl-code-issues
+			     csharp-omnisharp-curl-semantic-errors)))
 
 (defun omnisharp--init-imenu-support ()
   (when omnisharp-imenu-support
