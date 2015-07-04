5c84ca2169ce60f4b5ed84dfb67144af43006763
Mika Vilpas <mika.vilpas@gmail.com>
2015-07-04 09:14:03 +0300

    Fix build failing on not installing libuv for omnisharp-roslyn

---
+++ travis-stuff/install-mono.sh
@@ -0,0 +1,18 @@
+#! /bin/bash -e
+# Install mono and libuv. These are dependencies of omnisharp-roslyn
+# http://docs.asp.net/en/latest/getting-started/installing-on-linux.html
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+sudo apt-get update
+sudo apt-get install mono-complete
+
+sudo apt-get install automake libtool curl
+curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | sudo tar zxfv - -C /usr/local/src
+cd /usr/local/src/libuv-1.4.2
+sudo sh autogen.sh
+sudo ./configure
+sudo make
+sudo make install
+sudo rm -rf /usr/local/src/libuv-1.4.2 && cd ~/
+sudo ldconfig
