383977132a026c27d39958ccd68703dfb34a0c28
Mika Vilpas <mika.vilpas@gmail.com>
2015-07-10 20:14:43 +0300

    Start implementing support for reading from the stdio server process

--- src/stdio-testing.el
+++ src/stdio-testing.el
@@ -9,32 +9,35 @@
 (defvar omnisharp--server-info
   (make-omnisharp--server-info
    ;; use a pipe for the connection instead of a pty
-   (let ((process-connection-type nil))
-     (start-process
-      "omnisharp-server"             ; process name
-      "omnisharp-server"             ; buffer name
-      "/home/mika/git/omnisharp-emacs/omnisharp-roslyn/omnisharp"
-      ;; "-v"
-      "-s" "/home/mika/git/omnisharp-emacs/test/MinimalSolution/" "--stdio"))
+   (let ((process-connection-type nil)
+         (process (start-process
+                   "omnisharp-server"             ; process name
+                   "omnisharp-server"             ; buffer name
+                   "/home/mika/git/omnisharp-emacs/omnisharp-roslyn/omnisharp"
+                   ;; "-v"
+                   "-s" "/home/mika/git/omnisharp-emacs/test/MinimalSolution/" "--stdio")))
+     (set-process-filter process 'omnisharp--handle-server-message)
+     (set-process-sentinel process 'omnisharp--server-process-sentinel)
+     (set-process-coding-system process (utf-8-unix . utf-8-unix))
+     process)
    1))
 
 (setq omnisharp-debug t)
 
-;;; send input
 (defun omnisharp--send-command-to-server (server-info api-name payload)
+  ;; make RequestPacket with request-id
+  ;; send request
+  ;; store response handler associated with the request id
   (-let* (((&alist :process process
                    :request-id request-id) server-info)
           (request (omnisharp--make-request-packet api-name
                                                    payload
                                                    request-id)))
     (when omnisharp-debug
-      (message "--> %s" (prin1-to-string request)))
+      (omnisharp--log (format "--> %s" (prin1-to-string request))))
 
     (setcdr (assoc :request-id server-info) (+ 1 request-id))
-
-    (if (process-running-child-p process)
-        (process-send-string process request)
-      (message "Error: server-info process has died."))))
+    (process-send-string process request)))
 
 (defun omnisharp--make-request-packet (api-name payload request-id)
   (let ((response (-concat payload `((Command . ,api-name)
@@ -48,17 +51,22 @@
 ;;; read output
 ;;;
 ;;; Plan:
-;;; make request-id
-;;; send request
-;;; store response handler associated with the request id
 ;;;
 ;;; read output, increment request-id
 ;;; log events to process-buffer
 ;;; when a response ("Type": "response") arrives, call function associated with that Request_seq
 ;;; also remove the response handler
+(defun omnisharp--handle-server-message (process message-part)
+  (when (buffer-live-p (process-buffer process))
+    (with-current-buffer (process-buffer process)
+      (let ((moving (= (point) (process-mark process))))
+        (save-excursion
+          ;; Insert the text, advancing the process marker.
+          (goto-char (process-mark process))
+          (insert (concat "\n" message-part "\n"))
+          (set-marker (process-mark process) (point)))
+        (if moving (goto-char (process-mark process)))))))
 
-;;;
-;;; (set-process-filter omnisharp--server-process omnisharp--server-process-filter)
 ;;; read async event?
 ;;; - how can I set up listeners for different types of messages?
 
