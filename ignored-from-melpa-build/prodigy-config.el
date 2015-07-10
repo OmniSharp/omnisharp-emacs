302289634bcbe0a2c13c0a1cbf6ad52176adca2f
Mika Vilpas <mika.vilpas@gmail.com>
2015-07-10 13:51:40 +0300

    Configure prodigy with a macro, much cleaner

--- ignored-from-melpa-build/prodigy-config.el
+++ ignored-from-melpa-build/prodigy-config.el
@@ -16,46 +16,35 @@
 (require 'prodigy)
 
 ;; omnisharp-emacs development hacks
-(setenv "PATH" (concat "/opt/monodevelop/bin/:"
-                       (getenv "PATH")
+(setenv "PATH" (concat (getenv "PATH")
                        ":/home/mika/.cask/bin"
                        ":/home/mika/bin/"))
 
-(let ((omni-dir "/home/mika/git/omnisharp-emacs/"))
-  (prodigy-define-service
-    :name "OmniSharpServer for integration tests"
-    :command (concat omni-dir "start-omnisharp-server-for-integration-tests.sh")
-    :cwd omni-dir
-    :stop-signal 'kill
-    :kill-process-buffer-on-stop t
-    :truncate-output 200
-    :tags '(omnisharp))
+(defmacro def-omnisharp-service (name command)
+  (let ((omni-dir "/home/mika/git/omnisharp-emacs/"))
+    `(prodigy-define-service
+       :name ,name
+       :command (concat ,omni-dir ,command)
+       :cwd ,omni-dir
+       :stop-signal 'kill
+       :kill-process-buffer-on-stop t
+       :truncate-output 200
+       :tags '(omnisharp))))
 
-  (prodigy-define-service
-    :name "omnisharp-emacs integration tests"
-    :command (concat omni-dir "run-integration-tests.sh")
-    :cwd omni-dir
-    :stop-signal 'kill
-    :kill-process-buffer-on-stop t
-    :truncate-output 200
-    :tags '(omnisharp))
+(def-omnisharp-service "OmniSharpServer for integration tests"
+  "start-omnisharp-server-for-integration-tests.sh")
 
-  (prodigy-define-service
-    :name "omnisharp-emacs unit tests"
-    :command (concat omni-dir "run-tests.sh")
-    :cwd omni-dir
-    :stop-signal 'kill
-    :kill-process-buffer-on-stop t
-    :truncate-output 200
-    :tags '(omnisharp))
+(def-omnisharp-service "omnisharp-emacs integration tests"
+  "run-integration-tests.sh")
 
-  (prodigy-define-service
-    :name "omnisharp-emacs installation test"
-    :command (concat omni-dir "run-melpa-build-test.sh")
-    :cwd omni-dir
-    :stop-signal 'kill
-    :kill-process-buffer-on-stop t
-    :truncate-output 200
-    :tags '(omnisharp)))
+(def-omnisharp-service "omnisharp-emacs single integration test"
+  "run-single-integration-test.sh")
+
+(def-omnisharp-service "omnisharp-emacs unit tests"
+  "run-tests.sh")
+
+(def-omnisharp-service "omnisharp-emacs installation test"
+  "run-melpa-build-test.sh")
 
 (add-to-list 'evil-emacs-state-modes 'prodigy-mode)
+(provide 'prodigy-config)
