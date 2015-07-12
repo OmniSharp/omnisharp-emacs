bcf222d7ff975ca6423d682cc9dd8a964bf97962
Mika Vilpas <mika.vilpas@gmail.com>
2015-07-12 13:38:28 +0300

    Implement reading full lines from consecutive partial stdio messages
    The omnisharp-roslyn stdio server's output will be sent from the server
    one line at a time, but emacs is forced to read it in chunks of some
    amount of characters. This will keep track of what characters were last
    sent from the server, and return full lines sent from the server.

--- src/stdio-testing.el
+++ src/stdio-testing.el
@@ -6,22 +6,6 @@
     ;; places.
     (:request-id . ,request-id)))
 
-(defvar omnisharp--server-info
-  (make-omnisharp--server-info
-   ;; use a pipe for the connection instead of a pty
-   (let ((process-connection-type nil)
-         (process (start-process
-                   "omnisharp-server"             ; process name
-                   "omnisharp-server"             ; buffer name
-                   "/home/mika/git/omnisharp-emacs/omnisharp-roslyn/omnisharp"
-                   ;; "-v"
-                   "-s" "/home/mika/git/omnisharp-emacs/test/MinimalSolution/" "--stdio")))
-     (set-process-filter process 'omnisharp--handle-server-message)
-     (set-process-sentinel process 'omnisharp--server-process-sentinel)
-     (set-process-coding-system process (utf-8-unix . utf-8-unix))
-     process)
-   1))
-
 (setq omnisharp-debug t)
 
 (defun omnisharp--send-command-to-server (server-info api-name payload)
@@ -44,10 +28,6 @@
                                      (Seq . ,request-id)))))
     (concat (json-encode response) "\n")))
 
-(omnisharp--send-command-to-server omnisharp--server-info
-                                   "checkreadystatus"
-                                   nil)
-
 ;;; read output
 ;;;
 ;;; Plan:
@@ -57,17 +37,69 @@
 ;;; when a response ("Type": "response") arrives, call function associated with that Request_seq
 ;;; also remove the response handler
 (defun omnisharp--handle-server-message (process message-part)
+  (let ((messages-from-server (omnisharp--read-lines-from-process-output
+                               process message-part)))
+    messages-from-server))
+
+(defun omnisharp--at-full-line? ()
+  ;; all platforms use \n as newline in emacs
+  (s-ends-with? "\n"
+                (substring-no-properties (or (thing-at-point 'line)
+                                             ""))))
+
+(defun omnisharp--marker-at-full-line? (marker)
+  (save-excursion
+    (goto-char marker)
+    (omnisharp--at-full-line?)))
+
+(defun omnisharp--read-lines-from-process-output (process message-part)
+  "Problem: emacs reads output from the omnisharp-roslyn subprocess
+not line by line, but by some amount of characters. The way we want
+to read the omnisharp-roslyn output is line by line, since each
+response seems to be exactly one line long.
+
+This function returns full lines returned from the server process that
+have not been returned before."
   (when (buffer-live-p (process-buffer process))
     (with-current-buffer (process-buffer process)
-      (let ((moving (= (point) (process-mark process))))
+      ;; previous-text-marker will change if it refers to the marker
+      ;; and the marker is changed. Get it as an integer instead to
+      ;; avoid mutation
+      (let* ((previous-text-marker (save-excursion
+                                     (goto-char (process-mark process))
+                                     (point)))
+             (previous-text-was-complete-line?
+              (omnisharp--marker-at-full-line? previous-text-marker)))
         (save-excursion
-          ;; Insert the text, advancing the process marker.
-          (goto-char (process-mark process))
-          (insert (concat "\n" message-part "\n"))
-          (set-marker (process-mark process) (point)))
-        (if moving (goto-char (process-mark process)))))))
+          (progn
+            ;; Insert the text, advancing the process marker.
+            (goto-char previous-text-marker)
+            (insert message-part)
+            (set-marker (process-mark process) (point))
+            (when (omnisharp--marker-at-full-line? (process-mark process))
+              (progn
+                ;; get the start of the last inserted line
+                (goto-char previous-text-marker)
+                (beginning-of-line)
+                (s-lines (buffer-substring-no-properties
+                          (point) (process-mark process)))))))))))
 
-;;; read async event?
+(defvar omnisharp--server-info
+  (make-omnisharp--server-info
+   ;; use a pipe for the connection instead of a pty
+   (let ((process-connection-type nil)
+         (process (start-process
+                   "omnisharp-server"             ; process name
+                   "omnisharp-server"             ; buffer name
+                   "/home/mika/git/omnisharp-emacs/omnisharp-roslyn/omnisharp"
+                   ;; "-v"
+                   "-s" "/home/mika/git/omnisharp-emacs/test/MinimalSolution/" "--stdio")))
+     (set-process-filter process 'omnisharp--handle-server-message)
+     (set-process-sentinel process 'omnisharp--server-process-sentinel)
+     (set-process-coding-system process 'utf-8-unix 'utf-8-unix)
+     process)
+   1))
+
 ;;; - how can I set up listeners for different types of messages?
 
 ;;; todo stop-process
---
+++ test/stdio-test.el
@@ -0,0 +1,87 @@
+(omnisharp--send-command-to-server omnisharp--server-info
+                                   "checkreadystatus"
+                                   nil)
+
+(defmacro with-test-omnisharp-roslyn-process (process-symbol &rest test-forms)
+  `(unwind-protect
+       (progn
+         (let ((p (start-process "mock-omnisharp-roslyn-test-process"
+                                 "mock-omnisharp-roslyn-test-process" ; buffer name
+                                 "cat")))
+           (with-current-buffer "mock-omnisharp-roslyn-test-process"
+             (erase-buffer))
+           ;; allow using the process outside this macro
+           (setq ,process-symbol p)
+           ,@test-forms))
+     (kill-process "mock-omnisharp-roslyn-test-process")))
+
+(ert-deftest omnisharp--read-lines-from-process-output-test ()
+  ;; A single message in its entirety. Should return the message.
+  (with-test-omnisharp-roslyn-process
+   process
+   (should (equal '("{Some: Json}" "")
+                  (omnisharp--read-lines-from-process-output
+                   process
+                   ;; fake json does not confuse emacs syntax
+                   ;; highlighting when editing
+                   "{Some: Json}\n"))))
+
+  ;; Partial message. Should return nothing. The server will send
+  ;; another message, which will include the rest of this message, and
+  ;; the next call will return this line and any new lines added in
+  ;; that call.
+  (with-test-omnisharp-roslyn-process
+   process
+   (should (equal '()
+                  (omnisharp--read-lines-from-process-output
+                   process
+                   ;; notice no newline at the end! This means the
+                   ;; message is divided into more parts
+                   "{This message would continue in the next part..."))))
+
+  ;; A response message arriving in two parts. Should return the full
+  ;; line and the next call should return the partial line and any new
+  ;; lines added in that call. So the messages arriving are:
+  ;; 1. Message start
+  ;; 2. Message end
+  (with-test-omnisharp-roslyn-process
+   process
+   (progn
+     (omnisharp--read-lines-from-process-output
+      process
+      "{Message start")
+     (should (equal
+              '("{Message start, and message end}" "")
+              (omnisharp--read-lines-from-process-output
+               process
+               ", and message end}\n")))))
+
+  ;; Two lines arriving like this:
+  ;; 1. Message 1 part a
+  ;; 2. Message 1 part b & message 2
+  (with-test-omnisharp-roslyn-process
+   process
+   (progn
+     (omnisharp--read-lines-from-process-output process "{Message start")
+     (should (equal
+              '("{Message start, message end}"
+                "{Second message}"
+                "")
+              (omnisharp--read-lines-from-process-output
+               process
+               ", message end}\n{Second message}\n")))))
+
+  ;; 1. Full message with partial message
+  ;; 2. The rest of the partial message.
+  ;; The first full message will be returned with the first call, and
+  ;; the partial message will be returned after the second call.
+  (with-test-omnisharp-roslyn-process
+   process
+   (progn
+     (omnisharp--read-lines-from-process-output process "{First message}\n{Second message start")
+     (should (equal
+              '("{Second message start, second message end}"
+                "")
+              (omnisharp--read-lines-from-process-output
+               process
+               ", second message end}\n"))))))
