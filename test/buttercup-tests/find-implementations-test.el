4804b94b8d096fe2550368bef1ae9bc2f306021f
Mika Vilpas <mika.vilpas@gmail.com>
2015-07-17 09:11:22 +0300

    Uncomment a "show implementation" test

--- test/buttercup-tests/find-implementations-test.el
+++ test/buttercup-tests/find-implementations-test.el
@@ -11,14 +11,14 @@
     (ot--wait-for-seconds 1)
     (ot--point-should-be-on-a-line-containing "public class SomeClass : BaseClass {}"))
 
-  (comment (it "shows a list of implementations when more than one found"
-             (ot--buffer-contents-and-point-at-$
-              "public class Base$Class {}
+  (it "shows a list of implementations when more than one found"
+      (ot--buffer-contents-and-point-at-$
+       "public class Base$Class {}
       public class SomeClass : BaseClass {}
       public class SomeClass2 : BaseClass {}")
 
-             (ot--evaluate-and-wait-for-server-response "(omnisharp-find-implementations)")
-             (ot--wait-for-seconds 1)
-             (ot--switch-to-buffer "* OmniSharp : Implementations *")
-             (ot--i-should-see "public class SomeClass : BaseClass {}")
-             (ot--i-should-see "public class SomeClass2 : BaseClass {}"))))
+      (ot--evaluate-and-wait-for-server-response "(omnisharp-find-implementations)")
+      (ot--wait-for-seconds 1)
+      (ot--switch-to-buffer "* OmniSharp : Implementations *")
+      (ot--i-should-see "public class SomeClass : BaseClass {}")
+      (ot--i-should-see "public class SomeClass2 : BaseClass {}")))
