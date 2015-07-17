1073e9c35e7f2eed011d3a8dc323e211cc443822
Mika Vilpas <mika.vilpas@gmail.com>
2015-07-17 20:26:41 +0300

    Add an integration test for renaming across files

--- test/buttercup-tests/rename-symbol-test.el
+++ test/buttercup-tests/rename-symbol-test.el
@@ -26,4 +26,50 @@ namespace minimal
     public class OtherClass {
         OldClassChanged foo; // rename here
     }
-}")))
+}"))
+
+  (it "renames a symbol referenced in multiple files"
+    (ot--open-the-minimal-solution-source-file "MyClass.cs")
+    (ot--buffer-contents-and-point-at-$
+     "using System;
+      namespace minimal$
+      {
+          public class MyClass {}
+      }")
+
+    (ot--open-the-minimal-solution-source-file "MyClassContainer.cs")
+    (ot--buffer-contents-and-point-at-$
+     "using System;
+      namespace minimal
+      {
+          public class MyClassContainer
+          {
+              public My$Class foo;
+          }
+      }")
+
+    (ot--keyboard-input
+     (ot--press-key "M-x")
+     (ot--type "omnisharp-rename")
+     (ot--press-key "RET")
+     ;; The new name will be MyClass2
+     (ot--type "2")
+     (ot--press-key "RET"))
+
+    (ot--buffer-should-contain
+     "using System;
+      namespace minimal
+      {
+          public class MyClassContainer
+          {
+              public MyClass2 foo;
+          }
+      }")
+
+    (ot--switch-to-buffer "MyClass.cs")
+    (ot--buffer-should-contain
+     "using System;
+      namespace minimal
+      {
+          public class MyClass2 {}
+      }")))
