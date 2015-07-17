abe29d726d61d2e1d0dda94e840f2f6847601d3b
Mika Vilpas <mika.vilpas@gmail.com>
2015-07-17 23:16:00 +0300

    Don't install libuv on travis

--- travis-stuff/install-mono.sh
+++ travis-stuff/install-mono.sh
@@ -14,12 +14,4 @@ curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX
 # should show help text if installed successfully
 dnvm
 
-sudo apt-get install automake libtool curl
-curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | sudo tar zxfv - -C /usr/local/src
-cd /usr/local/src/libuv-1.4.2
-sudo sh autogen.sh
-sudo ./configure
-sudo make
-sudo make install
-sudo rm -rf /usr/local/src/libuv-1.4.2 && cd ~/
-sudo ldconfig
+ls -alR ~/.dnx
