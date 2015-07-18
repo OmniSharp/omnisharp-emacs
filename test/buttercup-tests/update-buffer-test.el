d559f84651a944cb2b2009d848d23274dba29bf8
Mika Vilpas <mika.vilpas@gmail.com>
2015-07-18 08:21:20 +0300

    Remove updatebuffer debug shenanigans
    Is that how it's spelled?

--- test/buttercup-tests/update-buffer-test.el
+++ test/buttercup-tests/update-buffer-test.el
@@ -1,30 +1,8 @@
 (describe "Update buffer"
   (before-each
-    (condition-case error-symbol
-        (progn
-          (ot--open-the-minimal-solution-source-file "MyClass.cs")
-          (ot--buffer-contents-and-point-at-$
-           "//contents don't matter")
-          (progn
-            (with-current-buffer "*omnisharp-debug*"
-              ;; If there is a problem with the server, it will show in
-              ;; this trivial test case. Better view all logs to
-              ;; inspect.
-              (print "\n\n\n*omnisharp-debug* buffer contents:\n")
-              (print (buffer-string)))
-            (with-current-buffer "Omni-Server"
-              (print "\n\n\nOmni-Server buffer contents:\n")
-              (print (buffer-string)))))
-      (error (progn
-               (with-current-buffer "*omnisharp-debug*"
-                 ;; If there is a problem with the server, it will show in
-                 ;; this trivial test case. Better view all logs to
-                 ;; inspect.
-                 (print "\n\n\n*omnisharp-debug* buffer contents:\n")
-                 (print (buffer-string)))
-               (with-current-buffer "Omni-Server"
-                 (print "\n\n\nOmni-Server buffer contents:\n")
-                 (print (buffer-string)))))))
+    (ot--open-the-minimal-solution-source-file "MyClass.cs")
+    (ot--buffer-contents-and-point-at-$
+     "//contents don't matter"))
 
   (it "does not crash when called"
     ;; no easy way to verify the server has done its thing
