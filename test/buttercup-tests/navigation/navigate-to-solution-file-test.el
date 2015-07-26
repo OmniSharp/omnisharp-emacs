9fe0f282cf7774f894ef67d28d805dbf527bff8d
Mika Vilpas <mika.vilpas@gmail.com>
2015-07-26 14:27:48 +0300

    Fix test

--- test/buttercup-tests/navigation/navigate-to-solution-file-test.el
+++ test/buttercup-tests/navigation/navigate-to-solution-file-test.el
@@ -5,9 +5,8 @@
 
     (spy-on 'omnisharp--choose-quickfix-ido :and-call-fake
             (lambda (quickfixes)
-              (let ((myclass-file (--first (s-contains? "MyClass.cs" (cdr (assoc 'Text it)))
-                                           quickfixes)))
-                (omnisharp-go-to-file-line-and-column myclass-file))))
+              (--first (s-contains? "MyClass.cs" (cdr (assoc 'Text it)))
+                       quickfixes)))
     (omnisharp--wait-until-request-completed
      (omnisharp-navigate-to-solution-file))
     (ot--there-should-be-a-window-editing-the-file "MyClass.cs")))
