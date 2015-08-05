e8c79a3137023337d7797c0e5b5e15b43535e16d
nosami <jasonimison@gmail.com>
2015-08-05 16:33:43 +0100

    updated test

--- test/buttercup-tests/eldoc/eldoc-test.el
+++ test/buttercup-tests/eldoc/eldoc-test.el
@@ -11,9 +11,10 @@
                System.Console.Write$Line
            }")
 
-      (view-echo-area-messages)
-      (goto-char (point-max))
-      (goto-char (point-min))
-      (forward-line (1- count))
+      (ot--press-key "<left>")
+      ;; (view-echo-area-messages)
+      ;; (goto-char (point-max))
+      ;; (goto-char (point-min))
+      ;; (forward-line (1- count))
       (expect (read-string) :to-equal "void Console.WriteLine()")))
 
