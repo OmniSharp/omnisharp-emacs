653ed1d951f993e901f73e1da2333f4758e63e1e
Mika Vilpas <mika.vilpas@gmail.com>
2015-08-06 08:48:25 +0300

    Update current-type-information-test format

--- test/buttercup-tests/current-symbol/current-type-information-test.el
+++ test/buttercup-tests/current-symbol/current-type-information-test.el
@@ -1,15 +1,12 @@
 (describe "Current type information"
-
-  (before-each
-    (ot--open-the-minimal-solution-source-file "MyClassContainer.cs")
-    (spy-on 'message :and-call-through))
-
   (it "lists usages of the symbol under point"
+    (ot--open-the-minimal-solution-source-file "MyClassContainer.cs")
+    (spy-on 'message :and-call-through)
     (ot--buffer-contents-and-point-at-$
      "namespace minimal"
      "{"
      "    public class Tar$get {}"
      "}")
 
-    (ot--evaluate-and-wait-for-server-response "(omnisharp-current-type-information)")
+    (omnisharp--wait-until-request-completed (omnisharp-current-type-information))
     (expect 'message :to-have-been-called-with "minimal.Target")))
