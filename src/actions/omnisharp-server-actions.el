85dddb0a73dfe7b3c9e1c6cdba30ced2c7335ba2
Mika Vilpas <mika.vilpas@gmail.com>
2016-04-16 08:45:50 +0300

    Report which omnisharp server is used

--- src/actions/omnisharp-server-actions.el
+++ src/actions/omnisharp-server-actions.el
@@ -10,7 +10,9 @@
   (unless (bound-and-true-p omnisharp-server-executable-path)
     (error "Could not find the OmniSharp executable. Please set the variable omnisharp-server-executable-path to a valid path"))
   (if (file-directory-p path-to-project)
-      (message (format "Starting OmniSharpServer using project folder: %s" path-to-project))
+      (progn
+        (message (format "Starting OmniSharpServer using project folder: %s" path-to-project))
+        (message "using the server at: %s" omnisharp-server-executable-path))
     (error (format "Path does not lead to a valid project folder path: %s" path-to-project)))
   (if (get-buffer BufferName)
       (kill-buffer BufferName))
