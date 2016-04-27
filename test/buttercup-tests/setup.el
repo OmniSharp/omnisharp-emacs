a418e7b12803f068d56060e8eb8ef017b04242cd
Mika Vilpas <mika.vilpas@gmail.com>
2016-04-27 20:34:46 +0300

    Use omnisharp-roslynv1.9-alpha13.tar.gz in integration tests

--- test/buttercup-tests/setup.el
+++ test/buttercup-tests/setup.el
@@ -244,10 +244,10 @@ with one."
            (lambda (_prompt _quickfixes)
              (funcall ,answer-function _quickfixes))))
 
-;;; Test suite setup. Start a test server process that can be used by
-;;; all tests
+;; Test suite setup. Start a test server process that can be used by
+;; all tests
 (let ((omnisharp-server-executable-path (concat omnisharp-emacs-root-path
-                                                "/travis-stuff/omnisharp-roslyn/run")))
+                                                "/travis-stuff/omnisharp-roslyn/OmniSharp")))
   (omnisharp--create-ecukes-test-server omnisharp-emacs-root-path))
 ;; wait that the server is alive and ready before starting the test run
 (with-timeout (2 ; seconds
