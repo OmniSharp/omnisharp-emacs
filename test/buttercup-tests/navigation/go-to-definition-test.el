ac0f4067e5c491d6164f4de736c842af35f73d27
Mika Vilpas <mika.vilpas@gmail.com>
2016-05-09 22:04:20 +0300

    Re-enable "goes to member defined in another file" test
    Don't test using another window, this seems to be a bit unreliable in
    emacs.

--- test/buttercup-tests/navigation/go-to-definition-test.el
+++ test/buttercup-tests/navigation/go-to-definition-test.el
@@ -15,7 +15,7 @@
     (ot--point-should-be-on-a-line-containing "public class Target {}"))
 
 
-  (xit "goes to a member defined in another file using another window"
+  (it "goes to a member defined in another file"
     ;; We have to let the server know the contents of the files before
     ;; doing anything, otherwise the contents might not be what they
     ;; are on disk
@@ -43,9 +43,6 @@
      "    }"
      "}")
     (omnisharp--wait-until-request-completed (omnisharp-go-to-definition-other-window))
-
-    (ot--there-should-be-a-window-editing-the-file "MyClassContainer.cs")
-    (ot--there-should-be-a-window-editing-the-file "MyClass.cs")
 
     (ot--switch-to-the-window-in-the-buffer "MyClass.cs")
     (ot--point-should-be-on-a-line-containing "public class MyClass")))
