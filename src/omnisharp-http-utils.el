aea8ba6174968345d9224dc459e32b67a4a70b2a
jtbm37 <jean@tbm.email>
2016-05-15 14:40:22 +0100

    Add more debugging messages

--- src/omnisharp-http-utils.el
+++ src/omnisharp-http-utils.el
@@ -11,15 +11,18 @@
 ;;;###autoload
 (defun omnisharp-post-http-message (url &optional params)
   "Post http request to server. Return result."
-  (let* ((response (omnisharp--submit-request url params))
+  (let* ((url (omnisharp--get-api-url url))
+         (response (omnisharp--submit-request url params))
          (data (elt response 3)))
     (unless data
-      (message "Error when sending request"))
+      (message "Error when sending request %s" url))
+    (when omnisharp-debug
+      (message "Response:\n%s" (prin1-to-string data)))
     data))
 
 (defun omnisharp--submit-request (url &optional params)
   (require 'request)
-  (request (omnisharp--get-api-url url)
+  (request url
            :type "POST"
            :parser 'json-read
            :sync t
@@ -27,9 +30,9 @@
            :error
            (cl-function (lambda (&rest args &key error-thrown &allow-other-keys)
                           (message "Got error: %S" error-thrown)))
-           :complete (lambda (&rest _) (message "Finished!"))
+           :complete (lambda (&rest _) (when omnisharp-debug (message "Request completed")))
            :success (cl-function
-                     (lambda (&key data &allow-other-keys) (message "Received")
+                     (lambda (&key data &allow-other-keys) (when omnisharp-debug (message "Request succeeded"))
                        ))))
 
 (defun omnisharp--server-process-sentinel (process event)
