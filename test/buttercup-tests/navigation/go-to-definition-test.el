00bd7a1121f22f4b51385e5dd242643fbbda73cf
Saulius Menkevičius <saulius.menkevicius@gmail.com>
2017-08-03 23:39:52 -0600

    Implementation for go-to-definition for types in metadata.
    This makes it possible to navigate to types that don't have source, e.g.
    CLR, external DLLs, nuget packages, etc.

--- test/buttercup-tests/navigation/go-to-definition-test.el
+++ test/buttercup-tests/navigation/go-to-definition-test.el
@@ -1,4 +1,16 @@
-;; License: GNU General Public License version 3, or (at your option) any later version
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 (describe "Go to definition"
   (it "goes to definition in the same file"
@@ -47,5 +59,25 @@
     (omnisharp--wait-until-request-completed (omnisharp-go-to-definition-other-window))
 
     (ot--switch-to-the-window-in-the-buffer "MyClass.cs")
-    (ot--point-should-be-on-a-line-containing "public class MyClass")))
+    (ot--point-should-be-on-a-line-containing "public class MyClass"))
 
+  (it "goes to a member defined in metadata"
+      (ot--open-the-minimal-project-source-file "MyClass.cs")
+      (ot--buffer-contents-and-point-at-$
+       "using System;"
+       "namespace minimal"
+       "{"
+       "    public class MyClass"
+       "    {"
+       "        public st$ring foo;"
+       "    }"
+       "}")
+
+      (omnisharp-go-to-definition)
+      (ot--wait-until-all-requests-completed)
+
+      ;; TODO: for some reason I need to set current buffer from window list
+      ;;       with with-current-buffer..
+      (with-current-buffer (car (mapcar #'window-buffer (window-list)))
+        (ot--i-should-be-in-buffer-name "*omnisharp-metadata:MinimalProject:System.Runtime:System.String*")
+        (ot--point-should-be-on-a-line-containing "public sealed class String"))))
--- test/buttercup-tests/navigation/navigate-to-solution-file-test.el
+++ test/buttercup-tests/navigation/navigate-to-solution-file-test.el
@@ -1,4 +1,17 @@
 ;; License: GNU General Public License version 3, or (at your option) any later version
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 (describe "Navigate to solution file"
   (it "asks the user for a file in the current solution to navigate to and goes there"
