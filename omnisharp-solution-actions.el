f05cf1061910984b9236cbceebacdf324705fa33
Saulius Menkevičius <saulius.menkevicius@gmail.com>
2017-12-26 17:37:04 +0200

    omnisharp-solution-actions.el: introduce omnisharp-solution-errors
    this will list all the errors/warnings from omnisharp for all the projects
    loaded in the solution, can take more than 1 seconds.
    
    Related to https://github.com/OmniSharp/omnisharp-emacs/issues/414,
    ("View all errors in solution")

--- omnisharp-solution-actions.el
+++ omnisharp-solution-actions.el
@@ -14,6 +14,69 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+(defun omnisharp--prepare-solution-errors-buffer ()
+  "Makes a new *omnisharp-solution-errors* buffer or creates a new one
+and enabled compilation-mode on the buffer."
+  (let ((existing-buffer (get-buffer "*omnisharp-solution-errors*"))
+        (solution-root-dir (cdr (assoc :project-root omnisharp--server-info))))
+    (if existing-buffer
+        (progn
+          (with-current-buffer existing-buffer
+            (setq buffer-read-only nil)
+            (erase-buffer)
+            (setq buffer-read-only t)
+            (setq default-directory solution-root-dir))
+          existing-buffer)
+      (let ((buffer (get-buffer-create "*omnisharp-solution-errors*")))
+        (with-current-buffer buffer
+          (setq default-directory solution-root-dir)
+          (compilation-mode)
+          buffer)))))
+
+(defun omnisharp-solution-errors (&optional errors-only)
+  "Opens a new buffer *omnisharp-solution-errors* (or updates existing one)
+with solution errors. This is the same error list as emitted by flycheck only
+for the whole solution."
+  (interactive "P")
+  (if omnisharp--server-info
+      ;; we want to show solutions error buffer early
+      (let ((buffer (omnisharp--prepare-solution-errors-buffer))
+            (time-started (current-time)))
+        (with-current-buffer buffer
+          (setq buffer-read-only nil)
+          (insert "omnisharp-solution-errors: waiting for omnisharp server ...")
+          (setq buffer-read-only t))
+        (display-buffer buffer)
+
+        ;; actually invoke the request
+        (omnisharp--send-command-to-server
+         "codecheck"
+         `((FileName . nil))
+         (lambda (response)
+           (let ((buffer (omnisharp--prepare-solution-errors-buffer))
+                 (error-list (omnisharp--vector-to-list (cdr (assoc 'QuickFixes response))))
+                 (time-elapsed-seconds (time-to-seconds (time-subtract (current-time) time-started))))
+             (display-buffer buffer)
+             (save-window-excursion
+               (with-current-buffer buffer
+                 (setq buffer-read-only nil)
+                 (dolist (item error-list)
+                   (let ((log-level (if (string= (cdr (assoc 'LogLevel item)) "Error") "error" "warning"))
+                         (filename (string-remove-prefix (concat default-directory "/") (cdr (assoc 'FileName item))))
+                         (line (cdr (assoc 'Line item)))
+                         (col (cdr (assoc 'Column item)))
+                         (text (cdr (assoc 'Text item))))
+                     (if (or (not errors-only)
+                             (string= log-level "error"))
+                         (insert (concat filename
+                                         "(" (number-to-string line) "," (number-to-string col) "): "
+                                         log-level " CS0000" ": "
+                                         text
+                                         "\n")))))
+                 (insert (concat "\nomnisharp-solution-errors: finished, "
+                                 "took " (number-to-string time-elapsed-seconds) " seconds to complete.\n"))
+                 (setq buffer-read-only t)))))))))
+
 ;; TODO create omnisharp-add-to-solution that lets user choose which
 ;; file to add.
 (defun omnisharp-add-to-solution-current-file ()
